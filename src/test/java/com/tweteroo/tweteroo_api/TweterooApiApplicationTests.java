eb6826f1d89328eaa684ae13b50deaf0876620f7
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2023-01-17 11:21:11 -0300

    chore: setup project

---
+++ src/test/java/com/tweteroo/tweteroo_api/TweterooApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tweteroo.tweteroo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweterooApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
