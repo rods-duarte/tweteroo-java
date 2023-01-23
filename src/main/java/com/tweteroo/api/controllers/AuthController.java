6a02983eecd2999abb688d3b278184e2f12d0203
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2023-01-23 14:48:28 -0300

    fix: change signup route from /users/signup to /auth/sign-up

--- src/main/java/com/tweteroo/api/controllers/AuthController.java
+++ src/main/java/com/tweteroo/api/controllers/AuthController.java
@@ -1,32 +1,36 @@
 package com.tweteroo.api.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tweteroo.api.dto.UserDTO;
 import com.tweteroo.api.models.User;
 import com.tweteroo.api.services.UserService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping("/users")
-public class UsersController {
+@RequestMapping("/auth")
+public class AuthController {
 
     @Autowired
-    private UserService service;
+    private UserService userService;
 
-    @GetMapping
-    @ResponseStatus(code = HttpStatus.OK)
-    public ResponseEntity<List<User>> listAll() {
-        List<User> users = service.findAll();
+    @PostMapping("/sign-up")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public ResponseEntity<String> signup(@RequestBody @Valid UserDTO body) {
+        User user = new User(body);
+        userService.create(user);
 
-        return ResponseEntity.ok().body(users);
+        return ResponseEntity.created(null).body("Ok");
     }
+    
 }
--- src/main/java/com/tweteroo/api/controllers/TweetsController.java
+++ src/main/java/com/tweteroo/api/controllers/TweetsController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import com.tweteroo.api.services.UserService;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/tweets")
 public class TweetsController {
 
